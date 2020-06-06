a4baaf1fd4018d29d7a1193e519d522d334b8d68
Liu233w <Liu233w@users.noreply.github.com>
2020-06-06 20:52:09 +1000

    docs: update headers

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,7 @@
 /* @auto-file-header
  * Copyright (c) 2020 Shumin Liu and Contributors.
+ * This file is a part of Auto File Header
+ * Automatically update your file headers with an extendible config file!
  * Licensed under MIT. See LICENSE file in the project root for full license information.
  */
 
