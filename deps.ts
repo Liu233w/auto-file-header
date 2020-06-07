243ba3e1dda5304b1388692832f18026f7938543
Liu233w <Liu233w@users.noreply.github.com>
2020-06-07 15:38:26 +1000

    build: add version to deps.ts

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std/testing/asserts.ts";
+} from "https://deno.land/std@0.55.0/testing/asserts.ts";
 
 export {
   walk,
@@ -9,7 +9,7 @@ export {
   readFileStr,
   writeFileStr,
   EOL,
-} from "https://deno.land/std/fs/mod.ts";
+} from "https://deno.land/std@0.55.0/fs/mod.ts";
 
 export {
   relative,
@@ -17,10 +17,10 @@ export {
   normalizeGlob,
   basename,
   join as joinPath,
-} from "https://deno.land/std/path/mod.ts";
+} from "https://deno.land/std@0.55.0/path/mod.ts";
 
-export { parse as parseArgs } from "https://deno.land/std/flags/mod.ts";
+export { parse as parseArgs } from "https://deno.land/std@0.55.0/flags/mod.ts";
 
-export * as log from "https://deno.land/std/log/mod.ts";
+export * as log from "https://deno.land/std@0.55.0/log/mod.ts";
 
-export { default as lodashGet } from "https://deno.land/x/lodash/get.js";
+export { default as lodashGet } from "https://deno.land/x/lodash@4.17.15-es/get.js";
