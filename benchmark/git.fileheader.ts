757d78bfa9d0ba0c1c86957863c10006ae4f4ecb
Liu233w <Liu233w@users.noreply.github.com>
2020-06-07 18:02:07 +1000

    chore: update benchmark

--- benchmark/git.fileheader.ts
+++ benchmark/git.fileheader.ts
@@ -1,9 +1,12 @@
 import {
   work,
   within,
+  vc,
 } from "https://raw.githubusercontent.com/Liu233w/auto-file-header/master/mod.ts";
 
 work((cfg) => {
+  cfg.versionControl = vc.git();
+
   cfg.include = [
     ".c",
     ".h",
